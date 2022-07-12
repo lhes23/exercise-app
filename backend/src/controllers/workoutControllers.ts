e02d21ba0177eb043e6292c61b638ab9312fbb50
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-12 20:01:10 +0800

    change status code

--- backend/src/controllers/workoutControllers.ts
+++ backend/src/controllers/workoutControllers.ts
@@ -4,9 +4,9 @@ import Workout from "../models/workoutModels";
 export const getAllWorkouts = async (req: Request, res: Response) => {
   try {
     const workouts = await Workout.find().sort({ createdAt: -1 });
-    return res.status(201).json({ workouts });
+    return res.status(200).json(workouts);
   } catch (error: any) {
-    return res.status(401).json({ error: error.message });
+    return res.status(400).json({ error: error.message });
   }
 };
 
@@ -14,9 +14,9 @@ export const AddWorkout = async (req: Request, res: Response) => {
   try {
     const { title, reps, load } = req.body;
     const workout = await Workout.create({ title, reps, load });
-    return res.status(201).json(workout);
+    return res.status(200).json(workout);
   } catch (error: any) {
-    return res.status(401).json({ error: error.message });
+    return res.status(400).json({ error: error.message });
   }
 };
 
@@ -24,9 +24,9 @@ export const getWorkout = async (req: Request, res: Response) => {
   try {
     const _id = req.params.id;
     const workout = await Workout.find({ _id });
-    return res.status(201).json(workout);
+    return res.status(200).json(workout);
   } catch (error: any) {
-    return res.status(401).json({ error: error.message });
+    return res.status(400).json({ error: error.message });
   }
 };
 
@@ -38,9 +38,9 @@ export const updateWorkout = async (req: Request, res: Response) => {
       { _id },
       { title, reps, load }
     );
-    return res.status(201).json(workout);
+    return res.status(200).json(workout);
   } catch (error: any) {
-    return res.status(401).json({ error: error.message });
+    return res.status(400).json({ error: error.message });
   }
 };
 
@@ -49,9 +49,9 @@ export const deleteWorkout = async (req: Request, res: Response) => {
     const _id = req.params.id;
     await Workout.findByIdAndDelete({ _id });
     return res
-      .status(201)
+      .status(200)
       .json({ message: `Successfully Deleted Workout id : ${_id}` });
   } catch (error: any) {
-    return res.status(401).json({ error: error.message });
+    return res.status(400).json({ error: error.message });
   }
 };
