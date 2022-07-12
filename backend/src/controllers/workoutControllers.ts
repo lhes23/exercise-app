141695eafe432da0f03297cc3a5aa2bb1c094fff
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-12 18:14:31 +0800

    remove " inside .env

--- backend/src/controllers/workoutControllers.ts
+++ backend/src/controllers/workoutControllers.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from "express";
+import Workout from "../models/workoutModels";
 
 export const getAllWorkouts = async (req: Request, res: Response) => {
   try {
-    return res.status(201).json({ workouts: "Workouts" });
+    const workouts = await Workout.find();
+    return res.status(201).json({ workouts });
   } catch (error) {
-    return res.status(401).json({ error: "Error" });
+    return res.status(401).json({ error });
   }
 };
 
-export const AddWorkout = async (req: Request, res: Response) => {};
+export const AddWorkout = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
 
 export const getWorkout = async (req: Request, res: Response) => {};
 
