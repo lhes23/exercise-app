1a896579eba9b7bac998a0889a5f33af9f8f0421
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-12 18:25:22 +0800

    created updateWorkout and deleteWorkout

--- backend/src/controllers/workoutControllers.ts
+++ backend/src/controllers/workoutControllers.ts
@@ -30,6 +30,28 @@ export const getWorkout = async (req: Request, res: Response) => {
   }
 };
 
-export const updateWorkout = async (req: Request, res: Response) => {};
+export const updateWorkout = async (req: Request, res: Response) => {
+  try {
+    const _id = req.params.id;
+    const { title, reps, load } = req.body;
+    const workout = await Workout.findByIdAndUpdate(
+      { _id },
+      { title, reps, load }
+    );
+    return res.status(201).json({ workout });
+  } catch (error) {
+    return res.status(401).json({ error });
+  }
+};
 
-export const deleteWorkout = async (req: Request, res: Response) => {};
+export const deleteWorkout = async (req: Request, res: Response) => {
+  try {
+    const _id = req.params.id;
+    const workout = await Workout.findByIdAndDelete({ _id });
+    return res
+      .status(201)
+      .json({ message: `Successfully Deleted Workout id : ${_id}` });
+  } catch (error) {
+    return res.status(401).json({ error });
+  }
+};
